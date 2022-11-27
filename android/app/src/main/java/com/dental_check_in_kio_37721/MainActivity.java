de4d5b580920302e9edf91f05a548f7b16d4c807
Crowdbotics <team@crowdbotics.com>
2022-11-27 20:46:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dental_check_in_kio_37721/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dental_check_in_kio_37721;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "dental_check_in_kio_37721";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "dental_check_in_kio_37721",
+  url: "https://dental_check_in_kio_37721.botics.co",
+  api: "https://dental_check_in_kio_37721.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
